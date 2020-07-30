c28119682c2f2a9c66aac9799538e4f3b6ff40db
YORYI <yoryi@MacBook-Air-de-YORYI.local>
2020-07-30 13:02:46 -0500

    modificando puertos agregando dependencias

--- scripts/start.js
+++ scripts/start.js
@@ -8,7 +8,7 @@ try {
   var merchantAPI = require('../index.js');
   merchantAPI.start({
     port: process.env.PORT || 3000,
-    bind: process.env.BIND || '127.0.0.1'
+    bind: process.env.BIND || '0.0.0.0'
   });
   if (process.env.RPC) {
     merchantAPI.startRPC({
